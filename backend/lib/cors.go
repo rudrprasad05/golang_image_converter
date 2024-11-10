635c3885573da49e649f52a91b4c7651bcc90fc8
Rudr <rudrprasad@yahoo.com>
2024-11-10 18:49:21 +1200

    ssl

--- backend/lib/cors.go
+++ backend/lib/cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		w.Header().Set("Access-Control-Allow-Origin", "https://imageconverter.rudrprasad.com")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
